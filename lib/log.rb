53730b11d8e9f29f2d500f5f94eaf8cd751cfb78
masuka <gergia@gmail.com>
2014-08-31 23:15:45 +0400

    Initial commit.

---
+++ lib/log.rb
@@ -0,0 +1,23 @@
+require 'logging'
+
+include Logging.globally :log
+
+Logging.appenders.stdout(:level => :debug)
+Logging.appenders.rolling_file(
+	'hc_service.log',
+	:level	=> 'debug',
+	:age    => 'daily',
+	:rol_by	=> 'date',
+	:keep 	=> 10,
+	:layout => Logging.layouts.pattern(:pattern => '[%d] %-5l -%c-: %m\n', :backtrace => true)
+)
+Logging.logger.root.add_appenders 'stdout', 'hc_service.log'
+
+log.info 'Logging intiated!'
+
+
+
+
+
+
+  
---
+++ test.rb
@@ -0,0 +1,15 @@
+require_relative 'lib/log' 
+require_relative 'test2' 
+
+module HCService
+	module Config
+		extend self
+
+		
+	end
+	
+end
+
+puts HCService::Config
+
+
